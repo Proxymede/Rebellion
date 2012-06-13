fd0404086c6b30bfa00a140f99ae136e3e1bc98d
Vincent SAMBLAS <vinxce@aeris-gaming.com>
2012-06-13 14:34:18 +0200

    WIP

--- src/systeme.h
+++ src/systeme.h
@@ -1,9 +1,9 @@
 /*
 ** systeme.h for  in /home/pinty_f//Desktop/perso/rebellion/src
-** 
+**
 ** Made by felix pinty
 ** Login   <pinty_f@epitech.net>
-** 
+**
 ** Started on  Mon Jun 11 15:03:02 2012 felix pinty
 ** Last update Tue Jun 12 15:00:09 2012 felix pinty
 */
@@ -19,9 +19,14 @@ class	systeme
  private:
   std::string		name;
   std::list<planete>	planetes;
+
  public:
   void	addPlanete(planete &);
   void	showPlanete();
+
+  systeme();
+  systeme(std::string name);
+  ~systeme();
 };
 
 #endif
